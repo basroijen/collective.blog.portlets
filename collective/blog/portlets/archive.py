8caa9cf91e1e79256ffb168fa357ef0f381277e8
basroijen <basroijen@gmail.com>
2012-11-08 11:11:47 +0100

    Choose root path for archive portlet
    Added ability to choose a root folder for the archive portlet. Also removed depth=1 so all items in subfolders will be shown.

--- collective/blog/portlets/archive.py
+++ collective/blog/portlets/archive.py
@@ -4,14 +4,15 @@ from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
 from Products.CMFCore.utils import getToolByName
 
-from Products.CMFPlone.i18nl10n import monthname_msgid
-
 from zope import schema
 from zope.formlib import form
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.blog.portlets.utils import find_assignment_context
 from collective.blog.portlets import _
+
+from plone.app.vocabularies.catalog import SearchableTextSourceBinder
+from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
 
 
 class IArchivePortlet(IPortletDataProvider):
@@ -22,22 +23,21 @@ class IArchivePortlet(IPortletDataProvider):
     same.
     """
 
-    header = schema.TextLine(title=_(u'Title of the portlet'),
-                             description=_(u'The text that will be shown as the title of the portlet'),
-                             required=False)
-    
     archive_view = schema.TextLine(title=_(u"Archive view"),
                                    description=_(u"The name of the archive view"),
                                    default=u'blog_view',
                                    required=True)
 
+    root = schema.Choice(
+        title=_(u"label_root_path", default=u"Root node"),
+        description=_(u'help_root_path',
+                      default=u"You may search for and choose a folder "
+                                "to act as the root of the list. "
+                                "Leave blank to use the folder where the portlet is created."),
+        required=False,
+        source=SearchableTextSourceBinder({'is_folderish': True},
+                                          default_query='path:'))
 
-    reversed = schema.Bool(title=_(u"Reverse year and month list"),
-                           description=_("When checked, the first shown year will be the current one, and then the previous ones. The same applies to the months"),
-                           default=False,
-                           )
-     
-    
 
 class Assignment(base.Assignment):
     """Portlet assignment.
@@ -48,18 +48,12 @@ class Assignment(base.Assignment):
 
     implements(IArchivePortlet)
 
-    header = u'Monthly archive'
     archive_view = u'blog_view'
-    reversed = False
 
-    def __init__(self, header=u'Monthly archive',
-                       archive_view=u'blog_view', 
-                       reversed=False):
-
-        self.header = header
+    def __init__(self, archive_view=u'blog_view', root=None):
         self.archive_view = archive_view
-        self.reversed = reversed
-        
+        self.root = root
+
     @property
     def title(self):
         """This property is used to give the title of the portlet in the
@@ -77,7 +71,7 @@ class Renderer(base.Renderer):
     """
 
     render = ViewPageTemplateFile('archive.pt')
-    
+
     def update(self):
         self._counts = {}
         catalog = getToolByName(self.context, 'portal_catalog')
@@ -92,16 +86,15 @@ class Renderer(base.Renderer):
         portal_types = site_properties.getProperty('blog_types', None)
         if portal_types == None:
             portal_types = ('Document', 'News Item', 'File')
-        
-        # Because of ExtendedPathIndex being braindead it's tricky (read:
-        # impossible) to get all subobjects for all folder, without also
-        # getting the folder. So we set depth to 1, which means we only get
-        # the immediate children. This is not a bug, but a lack of feature.
-        brains = catalog(path={'query': self.folder_path, 'depth': 1},
+
+        if self.data.root:
+            rootPath = getToolByName(self, 'portal_url').getPortalPath()
+            self.folder_path = rootPath + self.data.root
+        brains = catalog(path={'query': self.folder_path},
                          portal_type=portal_types)
         if not brains:
             return
-        
+
         # Count the number of posts per month:
         allmonths = {}
         for brain in brains:
@@ -121,29 +114,18 @@ class Renderer(base.Renderer):
             # Add this month:
             months = self._counts[year]
             months[month] = allmonths[year, month]
-            
-    def years(self):
-        items = sorted(self._counts.keys())
-        if self.data.reversed:
-            return reversed(items)
 
-        return items
-    
+    def years(self):
+        return sorted(self._counts.keys())
+
     def months(self, year):
         # sort as integers, return as strings
         _months = sorted([int(m) for m in self._counts[year].keys()])
-        items = [str(m) for m in _months]
-        if self.data.reversed:
-            return reversed(items)
+        return [str(m) for m in _months]
 
-        return items
-
-    def monthname(self, month):
-        return monthname_msgid(month)
-    
     def count(self, year, month):
         return self._counts[year][month]
-    
+
     def archive_url(self, year, month):
         return '%s/%s?year=%s&month=%s' % (self.folder_url,
                                            self.data.archive_view,
@@ -155,14 +137,15 @@ class AddForm(base.AddForm):
 
     This is registered in configure.zcml. The form_fields variable tells
     zope.formlib which fields to display. The create() method actually
-    constructs the assignment that is being added.
+2    constructs the assignment that is being added.
     """
     form_fields = form.Fields(IArchivePortlet)
+    form_fields['root'].custom_widget = UberSelectionWidget
 
     def create(self, data):
         return Assignment(**data)
-    
-    
+
+
 class EditForm(base.EditForm):
     """Portlet edit form.
 
@@ -170,3 +153,4 @@ class EditForm(base.EditForm):
     zope.formlib which fields to display.
     """
     form_fields = form.Fields(IArchivePortlet)
+    form_fields['root'].custom_widget = UberSelectionWidget
